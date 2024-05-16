94bc7ac5d398cdf21a8f0a9c9e0b71edf84fbc42
Nathan <decastronathan@gmail.com>
2024-05-16 08:12:53 +0200

    fix jump

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
+#region
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#endregion
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            Application.Quit();
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKeyDown(KeyCode.F5)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
