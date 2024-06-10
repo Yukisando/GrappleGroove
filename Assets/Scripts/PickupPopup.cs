317d8723b92e8e5806522760c3347de558119015
Nathan de Castro <decastronathan@gmail.com>
2024-06-10 11:05:35 +0200

    .

--- Assets/Scripts/PickupPopup.cs
+++ Assets/Scripts/PickupPopup.cs
@@ -11,29 +11,29 @@ public class PickupPopup : MonoBehaviour
     public float height = 100f;
     public float duration = 2f;
     RectTransform rectTransform;
-    
+
     float startY;
     TMP_Text text;
-    
+
     void Awake() {
         rectTransform = GetComponent<RectTransform>();
         text = GetComponent<TMP_Text>();
     }
-    
-    void Reset() {
-        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, startY);
-    }
-    
+
     void Start() {
         ScratchManager.I.onNewNode += ShowPopup;
         startY = rectTransform.anchoredPosition.y;
     }
-    
+
+    void Init() {
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, startY);
+    }
+
     [Button]
     void ShowPopup(string _info = "New info!") {
-        Reset();
+        Init();
         text.text = _info;
         Debug.Log($"{_info}");
-        rectTransform.LeanMoveY(height, duration).setEaseOutBounce().setOnComplete(Reset);
+        rectTransform.LeanMoveY(height, duration).setEaseOutBounce().setOnComplete(Init);
     }
 }
