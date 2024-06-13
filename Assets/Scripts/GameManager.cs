595aef605af4265b08586a75f4a4b6d781385445
Nathan <decastronathan@gmail.com>
2024-06-13 12:46:57 +0200

    fxied save and plabks only cllide with player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,60 +14,60 @@ public class GameManager : MonoBehaviour
     [SerializeField] Transform respawnPoint;
     [SerializeField] ScratchManager scratchManager;
     [SerializeField] InfoPopup infoPopup;
-
+    
     [Header("Settings")]
     [SerializeField] bool loadLastCheckpointOnStart = true;
     [SerializeField] KeyCode respawnKey = KeyCode.Q;
     [SerializeField] KeyCode restartKey = KeyCode.F5;
     [SerializeField] KeyCode quitKey = KeyCode.Escape;
-
+    
     [Header("Audio")]
     [SerializeField] AudioClip deathSound;
     [SerializeField] AudioClip checkpointSound;
     [SerializeField] AudioClip nodeSound;
-
+    
     CheckpointVolume[] checkpointVolumes;
     EmancipationVolume[] emancipationVolumes;
     KillVolume[] killVolumes;
     NodePickupVolume[] nodeVolumes;
     ResetVolume[] resetVolumes;
-
+    
     void Awake() {
         checkpointVolumes = FindObjectsByType<CheckpointVolume>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         resetVolumes = FindObjectsByType<ResetVolume>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         nodeVolumes = FindObjectsByType<NodePickupVolume>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         killVolumes = FindObjectsByType<KillVolume>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         emancipationVolumes = FindObjectsByType<EmancipationVolume>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-
+        
         foreach (var checkpointVolume in checkpointVolumes) {
             checkpointVolume.onEnterVolume += OnPlayerEnteredCheckpointVolume;
         }
-
+        
         foreach (var resetVolume in resetVolumes) {
             resetVolume.onEnterVolume += ResetPlayer;
         }
-
+        
         foreach (var nodeVolume in nodeVolumes) {
             nodeVolume.onEnterVolume += OnPlayerEnteredNodePickupVolume;
         }
-
+        
         foreach (var killVolume in killVolumes) {
             killVolume.onEnterVolume += OnPlayerEnteredKillVolume;
         }
-
+        
         foreach (var emancipationVolume in emancipationVolumes) {
             emancipationVolume.onEnterVolume += OnPlayerEnterEmancipationVolume;
         }
-
+        
         if (loadLastCheckpointOnStart) LoadLastCheckpoint();
     }
-
+    
     void Update() {
         if (Input.GetKeyDown(quitKey)) Application.Quit();
         if (Input.GetKeyDown(respawnKey)) ResetPlayer();
         if (Input.GetKeyDown(restartKey)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
+    
     void LoadLastCheckpoint() {
         var checkpointPosition = checkpointManager.LoadLastCheckpoint();
         if (checkpointPosition != Vector3.zero) {
@@ -75,11 +75,11 @@ public class GameManager : MonoBehaviour
             playerDependencies.rb.MovePosition(checkpointPosition);
         }
     }
-
+    
     void OnPlayerEnterEmancipationVolume(RopeType _ropeType) {
         playerDependencies.GetComponent<GrapplingHook>().DestroyRopes(_ropeType);
     }
-
+    
     void ResetPlayer() {
         playerDependencies.GetComponent<GrapplingHook>().DestroyRopes();
         playerDependencies.audioSourceTop.PlayOneShot(deathSound);
@@ -88,21 +88,22 @@ public class GameManager : MonoBehaviour
         playerDependencies.rb.MovePosition(respawnPoint.position);
         Debug.Log("Player got reset!");
     }
-
+    
     void OnPlayerEnteredCheckpointVolume(CheckpointVolume _v) {
         playerDependencies.audioSourceTop.PlayOneShot(checkpointSound);
         _v.gameObject.SetActive(false);
         checkpointManager.SaveCheckpoint(_v.transform.position);
+        respawnPoint.position = _v.transform.position;
         infoPopup.ShowPopup($"{_v.name} checkpoint reached!");
     }
-
+    
     void OnPlayerEnteredNodePickupVolume(NodeData _nodeData) {
         scratchManager.AddNode(_nodeData);
         infoPopup.ShowPopup($"Node collected: {_nodeData.id}");
         playerDependencies.audioSourceTop.PlayOneShot(nodeSound);
         Debug.Log("Node collected!");
     }
-
+    
     void OnPlayerEnteredKillVolume() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("Player died!");
