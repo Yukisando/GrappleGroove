ca601428b676c0fda9cc46ceb6129b70937f166f
Nathan <decastronathan@gmail.com>
2025-05-25 18:43:45 +0200

    new sound effects

--- Assets/_Scripts/AssetManager.cs
+++ Assets/_Scripts/AssetManager.cs
@@ -15,14 +15,31 @@ public class AssetManager : MonoBehaviour
     public AudioClip startTimerSound;
     public AudioClip stopTimerSound;
     public AudioClip timerTickSound;
-    public AudioClip splashSound;
     public AudioClip bumpSound;
+    public AudioClip objectSpawn;
 
     void Awake() {
         I = this;
     }
 
     public void PlayClip(AudioClip clip) {
-        AudioSource.PlayClipAtPoint(clip, GameManager.I.playerDependencies.transform.position, 1.0f);
+        if (clip == null) return;
+
+        var sourceGo = new GameObject("TempAudio") {
+            transform = {
+                position = GameManager.I.playerDependencies.transform.position,
+            },
+        };
+
+        var audioSource = sourceGo.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = 1f;
+        audioSource.spatialBlend = 1f; // 3D sound
+        audioSource.minDistance = 3f; // Louder when close
+        audioSource.maxDistance = 50f; // Still audible farther away
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+        audioSource.Play();
+
+        Destroy(sourceGo, clip.length);
     }
 }
--- Assets/_Scripts/SoundOnActivate.cs
+++ Assets/_Scripts/SoundOnActivate.cs
@@ -1,23 +1,14 @@
 #region
 
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 #endregion
 
-[RequireComponent(typeof(AudioSource))]
 public class SoundOnActivate : MonoBehaviour
 {
-    AudioSource audioSource;
-
-    void Awake() {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.spatialBlend = 1.0f;
-        audioSource.rolloffMode = AudioRolloffMode.Linear;
-        audioSource.minDistance = 1.0f;
-        audioSource.maxDistance = 20.0f;
-    }
-
+    [Optional] AudioClip clip;
     void OnEnable() {
-        audioSource.PlayOneShot(AssetManager.I.spawnClip);
+        AssetManager.I.PlayClip(clip?? AssetManager.I.objectSpawn);
     }
 }
